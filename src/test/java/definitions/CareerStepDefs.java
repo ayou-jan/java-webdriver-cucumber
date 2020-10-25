33b8806869fb98e3308c52fec7e6f14f45ea75e5
ayou-jan <hairartur@gmail.com>
2020-10-24 22:01:14 -0400

    @career2

--- src/test/java/definitions/CareerStepDefs.java
+++ src/test/java/definitions/CareerStepDefs.java
@@ -6,18 +6,21 @@ import cucumber.api.java.en.When;
 import pages.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.getData;
+import static support.TestContext.getDriver;
 
 public class CareerStepDefs {
 
-    CareerHeroku careerHeroku = new CareerHeroku();
-    LoginHeroku loginHeroku = new LoginHeroku();
+    CareersLanding careersLanding = new CareersLanding();
+    CareerLogin careerLogin = new CareerLogin();
     ListHeroku listHeroku = new ListHeroku();
     PositionHeroku positionHeroku = new PositionHeroku();
     RecruitList recruitList = new RecruitList();
+    NewPosition newPosition = new NewPosition();
 
     @And("I login as {string}")
     public void iLoginAs(String username) {
-        careerHeroku
+        careersLanding
                 .pressLogin()
                 .login(username);
     }
@@ -38,5 +41,33 @@ public class CareerStepDefs {
     @And("I verify {string} position is removed")
     public void iVerifyPositionIsRemoved(String position) {
         assertThat(recruitList.verifyExistence(position)).isFalse();
+    }
+
+    @When("I create new position")
+    public void iCreateNewPosition() throws InterruptedException {
+        listHeroku.goToRecruitList();
+        recruitList.goToNewPosition()
+        .fillFields("automation");
+        newPosition.submit();
+        //recruitList.waitUpdatedList();
+
+    }
+
+    @Then("I verify new position is created")
+    public void iVerifyNewPositionIsCreated() throws InterruptedException {
+        Thread.sleep(2000);
+        //getDriver().navigate().refresh();
+        assertThat(recruitList.checkPosition()).isTrue();
+    }
+
+
+    @When("I remove new position")
+    public void iRemoveNewPosition() {
+        recruitList.removePosition(getData("automation").get("Title"));
+    }
+
+    @And("I verify new position is removed")
+    public void iVerifyNewPositionIsRemoved() {
+        assertThat(recruitList.verifyExistence(getData("automation").get("Title"))).isFalse();
     }
 }
--- src/test/java/pages/RecruitList.java
+++ src/test/java/pages/RecruitList.java
@@ -3,6 +3,11 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import support.Loggable;
+import support.Screenshot;
+
+import java.util.List;
 
 import static support.TestContext.*;
 
@@ -11,21 +16,49 @@ public class RecruitList extends Page {
     @FindBy(xpath = "//div[@class='row']")
     private WebElement positionsArea;
 
+    @FindBy(xpath = "//h4[contains(text(),'New Position')]")
+    private WebElement addPosition;
+
     private WebElement getRemovePositionButton(String jobTitle) {
-        return getDriver().findElement(By.xpath("//div[@class='col-sm-12 col-md-4 col-lg-3']//h4[text()='" + jobTitle + "']/../../..//i[@class='fa fa-close']"));
+        return getDriver().findElement(By.xpath("//h4[text()='" + jobTitle + "']/../../..//i[@class='fa fa-close']"));
     }
 
+    @FindBy(xpath = "//div[@class='row']")
+    private List<WebElement> listOfPositions;
+
     private WebElement getPosition(String jobTitle) {
-        return getDriver().findElement(By.xpath("//div[@class='col-sm-12 col-md-4 col-lg-3']//h4[text()='" + jobTitle + "']"));
+        return getDriver().findElement(By.xpath("//h4[text()='" + jobTitle + "']"));
     }
 
     public void removePosition(String name) {
-        clickWithJS(getRemovePositionButton(name));
-        //getActions().moveToElement(getDriver().findElement(By.xpath("//div[@class='col-sm-12 col-md-4 col-lg-3']//h4[text()='Principal Automation Engineer']"))).click();
+        //clickWithJS(getRemovePositionButton(name));
+        getActions().moveToElement(getPosition(name)).perform();
+        getRemovePositionButton(name).click();
+        getWait().until(ExpectedConditions.invisibilityOf(getPosition(name)));
     }
 
     public Boolean verifyExistence(String name) {
         return positionsArea.getText().contains(name);
+    }
+
+    public NewPosition goToNewPosition() {
+        addPosition.click();
+        takeScreenshot();
+        return new NewPosition();
+    }
+
+    public void waitUpdatedList() {
+        waitForVisible(listOfPositions.get(1));
+    }
+
+    public boolean checkPosition() throws InterruptedException {
+        //Thread.sleep(2000);
+        for (int i = 0; i < listOfPositions.size(); i++) {
+            if (listOfPositions.get(i).getText().contains(getData("automation").get("Title"))) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
